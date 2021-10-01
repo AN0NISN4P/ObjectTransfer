0c367390bc60d026d37777cf0513ad544ece5208
AN0NISN4P <andreasyoungandersen@gmail.com>
2021-10-01 10:25:55 +0200

    We got a formatter!

--- src/JSONFormatter.java
+++ src/JSONFormatter.java
@@ -1,14 +1,9 @@
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
-public class JSONFormatter
-{
-    public static void main(String[] args)
-    {
-        Human human = new Human(20, "Black");
-        Gson gson = new GsonBuilder().setPrettyPrinting().create(); // pretty print
-        String prettyJson = gson.toJson(human);
-        System.out.println(prettyJson);
+public class JSONFormatter {
+	private static Gson formatter = new Gson();
 
-    }
+	public static String toJson(Object o) {
+		return formatter.toJson(o);
+	}
 }
