e069db90a282bfa818cee1cda23425938133ead5
SebastianBThomsen <280301@via.dk>
2021-10-01 10:12:02 +0200

    veri noice push

--- src/JSONFormatter.java
+++ src/JSONFormatter.java
@@ -1,3 +1,14 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class JSONFormatter
 {
+    public static void main(String[] args)
+    {
+        Human human = new Human(20, "Black");
+        Gson gson = new GsonBuilder().setPrettyPrinting().create(); // pretty print
+        String prettyJson = gson.toJson(human);
+        System.out.println(prettyJson);
+
+    }
 }
