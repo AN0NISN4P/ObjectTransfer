f576ee9a2ab95d9764ff793ac04a9d6d130953ac
SebastianBThomsen <280301@via.dk>
2021-10-01 10:59:10 +0200

    Veri Cool Frile Rider

--- src/JSONFormatter.java
+++ src/JSONFormatter.java
@@ -1,7 +1,8 @@
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class JSONFormatter {
-	private static Gson formatter = new Gson();
+	private static Gson formatter = new GsonBuilder().setPrettyPrinting().create(); // pretty print
 
 	public static String toJson(Object o) {
 		return formatter.toJson(o);
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
 	public static void main(String[] args) {
 
+		Human human = new Human(20, "Black");
+
+		WriteToFile.writeToFile(JSONFormatter.toJson(human));
+
+		System.out.println(JSONFormatter.toJson(human));
+
 	}
 }
