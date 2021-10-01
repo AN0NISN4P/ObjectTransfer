951e934ae16bfc842e18edaacc30f29e5400bb8a
AN0NISN4P <andreasyoungandersen@gmail.com>
2021-10-01 10:52:44 +0200

    updated filewriter

--- src/WriteToFile.java
+++ src/WriteToFile.java
@@ -2,15 +2,13 @@ import java.io.FileWriter;   // Import the FileWriter class
 import java.io.IOException;  // Import the IOException class to handle errors
 
 public class WriteToFile {
-    public static void main(String[] args) {
-        try {
-            FileWriter myWriter = new FileWriter("filename.txt");
-            myWriter.write("Files in Java might be tricky, but it is fun enough!");
-            myWriter.close();
-            System.out.println("Successfully wrote to the file.");
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-    }
+	public static void writeToFile(String txt) {
+		try {
+			FileWriter fw = new FileWriter("document.txt");
+			fw.write(txt);
+			fw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
